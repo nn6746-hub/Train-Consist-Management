4a0fb9f756e8421d064d0e02a286b675dd5eebdf
nn6746-hub <nn6746@srmist.edu.in>
2026-04-24 09:57:34 +0530

    [nn6746] Added [feature/uc7tcm]

---
+++ App/src/uc7.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String toString() {
+        return name + " -> " + capacity;
+    }
+}
+
+public class uc7 {
+
+    public static void main(String[] args) {
+
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+
+        System.out.println("Bogies sorted by capacity:");
+        for (Bogie b : bogies) {
+            System.out.println(b);
+        }
+    }
+}
